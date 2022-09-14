e01ca1f3ced9e110fb4689448f7dbe4eccacf1fd
Danna Ramos <ramos.danna.isw@unipolidgo.edu.mx>
2022-09-14 09:26:26 -0500

    Agregar archivos de proyecto.

---
+++ Actividad1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Actividad1
+{
+    class Program
+        
+    {
+      class Product
+        {
+            public decimal price { get; set; }
+            public decimal quantity { get; set; }
+            public string title { get; set;}
+            public  string descripion { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Actividad1/product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actividad1
+{
+    class product
+   
+    {
+        public decimal price { get; set; }
+        public decimal quantity { get; set; }
+        public string title { get; set; }
+        public string descripion { get; set; }
+    }
+
+}
+}
